227911cf8fbf76f9eae0109632f707d42d1aa904
dinhnguyencntt <dn1994099@gmail.com>
2024-02-26 09:26:01 +0700

    Add project files.

---
+++ ltcb tinh hieu/ltcb tinh hieu.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int so1, so2, hieu;
+	cout << "chuong trinh tính hieu\n";
+	cout << "nhap so thu nhat:";
+	cin >> so1;
+	cout << "nhap so thu hai:";
+	cin >> so2;
+	hieu = so1 - so2;
+	cout << so1 << "-" << "=" << hieu << endl;
+	return 0;
+}
